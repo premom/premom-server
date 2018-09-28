f499292853e4a52811efd364fb45816f87dd59e1
ohyeji <ohyeji@218.38.137.27>
2018-09-28 22:29:08 +0900

    first commit (batis test)

---
+++ premom/src/test/java/com/petifee/controller/MySQLConnTest.java
@@ -0,0 +1,26 @@
+package com.petifee.controller;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+
+public class MySQLConnTest {
+	private static final String Driver = "com.mysql.cj.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/test?useSSL=false&serverTimezone=Asia/Seoul";
+	private static final String User = "root";
+	private static final String Pass = "asdf7070!";
+	
+	@Test
+	public void testConnection() throws Exception {
+		Class.forName(Driver); //드라이버 지정 클래스 로딩
+		try (Connection con = DriverManager.getConnection(URL, User, Pass)){
+			System.out.println(con);
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
