bfd4202c89207716b895d9b213edd3ed9e85020c
ohyeji <ohyeji@172.20.10.3>
2018-11-09 11:05:24 +0900

    # tbl_seat car, sub_car 컬럼 추가

--- premom/src/main/java/com/petifee/domain/SeatVO.java
+++ premom/src/main/java/com/petifee/domain/SeatVO.java
@@ -5,18 +5,19 @@ import java.util.Date;
 public class SeatVO {
 	private int idx;
 	private int is_seat;
-	private String name;
 	private int fast_transfer;
+	private String car;
+	private String sub_car;
+	
 	
 	
 	public SeatVO(){
 	}
 
 	
-	public SeatVO(int idx, int is_seat, String name, int fast_transfer){
+	public SeatVO(int idx, int is_seat,  int fast_transfer){
 		this.idx = idx;
 		this.is_seat = is_seat;
-		this.name = name;
 		this.fast_transfer = fast_transfer;
 	}
 
@@ -37,14 +38,6 @@ public class SeatVO {
 	}
 
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
 	public int getFast_transfer() {
 		return fast_transfer;
 	}
@@ -53,5 +46,21 @@ public class SeatVO {
 		this.fast_transfer = fast_transfer;
 	}
 
+	public String getCar() {
+		return car;
+	}
+
+	public void setCar(String car) {
+		this.car = car;
+	}
+
+	public String getSub_car() {
+		return sub_car;
+	}
+
+	public void setSub_car(String sub_car) {
+		this.sub_car = sub_car;
+	}
+	
 
 }
